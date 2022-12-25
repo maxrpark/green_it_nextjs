3d0cf6d2be8522b69c3cda701515c4b9af9e817f
maxrpark <projectoindit@gmail.com>
2022-12-25 13:49:24 +0900

    https://green-it-server.onrender.com/

--- axios/index.ts
+++ axios/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseURL = "http://localhost:5000/api/v1";
+export const baseURL = "https://green-it-server.onrender.com/api/v1";
 
 export const productsFetch = axios.create({
   baseURL: baseURL + "/products",
