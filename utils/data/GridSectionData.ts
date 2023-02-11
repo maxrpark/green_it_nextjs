a91d54a5add3eaed9bb6ffa693b9044051ad0db2
maxrpark <projectoindit@gmail.com>
2023-02-11 11:29:05 +0900

    fix images

--- utils/data/GridSectionData.ts
+++ utils/data/GridSectionData.ts
@@ -2,25 +2,25 @@ export default [
   {
     id: 1,
     name: "Trees",
-    img: "https://res.cloudinary.com/dcytr1px3/image/upload/v1665878827/green_it/jgpzb8svvpmvhojx5wo1.jpg",
+    img: "https://res.cloudinary.com/dcytr1px3/image/upload/v1672304947/green_it/puofws4bomollm5mbnrz.jpg",
     gridStyles: "row-start-1 row-end-3 col-start-1 col-end-3",
   },
   {
     id: 2,
     name: "Flowers",
-    img: "https://res.cloudinary.com/dcytr1px3/image/upload/v1665878430/green_it/rst5zdweyb4ikxghkiz6.jpg",
+    img: "https://res.cloudinary.com/dcytr1px3/image/upload/v1672305292/green_it/xnejgsk79halzsuhc70b.jpg",
     gridStyles: "row-start-1 row-end-2 col-start-3 col-end-5",
   },
   {
     id: 3,
     name: "indoor",
-    img: "https://res.cloudinary.com/dcytr1px3/image/upload/v1665878481/green_it/n4q5ligkladspbrz36fc.jpg",
+    img: "https://res.cloudinary.com/dcytr1px3/image/upload/v1672273304/green_it/cygchauuwnissdvnbjgj.jpg",
     gridStyles: "row-start-2 row-end-3 col-start-3 col-end-6",
   },
   {
     id: 4,
     name: "cactus",
-    img: "https://res.cloudinary.com/dcytr1px3/image/upload/v1665878370/green_it/qluva4ctcox6myozmuis.jpg",
+    img: "https://res.cloudinary.com/dcytr1px3/image/upload/v1672305583/green_it/dbapd3xfzxnualmlgtre.jpg",
     gridStyles: "",
   },
   {
